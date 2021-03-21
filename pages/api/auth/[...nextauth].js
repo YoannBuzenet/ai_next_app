e1b432c4a880827d37e5eab083ad38f6bcdb4e97
Yoann <ybuzenet@gmail.com>
2021-03-21 07:52:18 +0100

    google auth works on nextjs app

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,41 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import customAdapter from "./custom_adapter/custom_adapter";
+import Adapters from "next-auth/adapters";
+
+const callbacks = {};
+callbacks.signIn = async function signIn(user, account, metadata) {
+  console.log("user", user);
+  console.log("account", account);
+  console.log("metadata", metadata);
+  if (account.provider === "google") {
+    const googleUser = {
+      id: metadata.id,
+      login: metadata.login,
+      name: metadata.name,
+      avatar: user.image,
+    };
+
+    user.accessToken = console.log("google user", googleUser);
+    return true;
+  }
+
+  return false;
+};
+
+callbacks.jwt = async function jwt(token, user) {
+  if (user) {
+    token = { accessToken: user.accessToken };
+  }
+
+  return token;
+};
+
+callbacks.session = async function session(session, token) {
+  // we can fetch info from back end here to add it to the session
+  session.accessToken = token.accessToken;
+  return session;
+};
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,5 +46,5 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  adapter: customAdapter,
+  callbacks,
 });
--- pages/api/auth/custom_adapter/custom_adapter.js
+++ pages/api/auth/custom_adapter/custom_adapter.js
@@ -1,36 +1,38 @@
 const Adapter = (config, options = {}) => {
   async function getAdapter(appOptions) {
+    console.log("bro-7");
     async function createUser(profile) {
+      console.log("bro-6");
       return null;
     }
 
     async function getUser(id) {
-      console.log("bro");
+      console.log("bro-5");
       return null;
     }
 
     async function getUserByEmail(email) {
-      console.log("bro");
+      console.log("bro-4");
       return null;
     }
 
     async function getUserByProviderAccountId(providerId, providerAccountId) {
-      console.log("bro");
+      console.log("bro-3");
       return null;
     }
 
     async function getUserByCredentials(credentials) {
-      console.log("bro");
+      console.log("bro-2");
       return null;
     }
 
     async function updateUser(user) {
-      console.log("bro");
+      console.log("bro-1");
       return null;
     }
 
     async function deleteUser(userId) {
-      console.log("bro");
+      console.log("bro0");
       return null;
     }
 
@@ -43,32 +45,32 @@ const Adapter = (config, options = {}) => {
       accessToken,
       accessTokenExpires
     ) {
-      console.log("bro");
+      console.log("bro1");
       return null;
     }
 
     async function unlinkAccount(userId, providerId, providerAccountId) {
-      console.log("bro");
+      console.log("bro2");
       return null;
     }
 
     async function createSession(user) {
-      console.log("bro");
+      console.log("bro3");
       return null;
     }
 
     async function getSession(sessionToken) {
-      console.log("bro");
+      console.log("bro4");
       return null;
     }
 
     async function updateSession(session, force) {
-      console.log("bro");
+      console.log("bro5");
       return null;
     }
 
     async function deleteSession(sessionToken) {
-      console.log("bro");
+      console.log("bro6");
       return null;
     }
 
@@ -79,12 +81,12 @@ const Adapter = (config, options = {}) => {
       secret,
       provider
     ) {
-      console.log("bro");
+      console.log("bro7");
       return null;
     }
 
     async function getVerificationRequest(identifier, token, secret, provider) {
-      console.log("bro");
+      console.log("bro8");
       return null;
     }
 
@@ -94,7 +96,7 @@ const Adapter = (config, options = {}) => {
       secret,
       provider
     ) {
-      console.log("bro");
+      console.log("bro9");
       return null;
     }
 
