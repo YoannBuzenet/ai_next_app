8787aef9f208e7c1215bf5c6913af24c554e26d2
Yoann <ybuzenet@gmail.com>
2021-03-26 17:52:14 +0100

    material UI icon button implemented for better UX

--- components/APIResult.jsx
+++ components/APIResult.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/APIResult.module.css";
 import FileCopyIcon from "@material-ui/icons/FileCopy";
+import IconButton from "@material-ui/core/IconButton";
 
 const APIResult = ({ index, initialText }) => {
   return (
     <div className={styles.oneResult}>
       <div className={styles.resulText}>{initialText}</div>
       <div className={styles.iconContainer}>
-        <FileCopyIcon className={styles.icons} />
+        <IconButton>
+          <FileCopyIcon className={styles.icons} />
+        </IconButton>
       </div>
     </div>
   );
