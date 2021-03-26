68de4d0141e1ad52a639558474736d96fa273663
Yoann <ybuzenet@gmail.com>
2021-03-26 09:58:41 +0100

    structure begin to look like something

--- components/CategorySelector.jsx
+++ components/CategorySelector.jsx
@@ -3,7 +3,19 @@ import Link from "next/link";
 import styles from "../styles/Category.module.css";
 
 const CategorySelector = ({ handleSelectCategory }) => {
-  return <>Select your category</>;
+  return (
+    <div className={styles.container}>
+      <h2>Select your category</h2>
+      <h3>Most Popular</h3>
+      <div className={styles.categoryPropositionsContainer}>
+        <p>Product Description</p>
+        <p>Value Proposition</p>
+        <p>Blog Title</p>
+        <p>Instagram Caption</p>
+        <p>Blog Intro</p>
+      </div>
+    </div>
+  );
 };
 
 export default CategorySelector;
