e04bb2f9a74916d741e1026e8f1d30002b64e1db
Yoann <ybuzenet@gmail.com>
2021-03-28 00:17:56 +0100

    css basis of pricing page

--- pages/pricing.js
+++ pages/pricing.js
@@ -4,13 +4,17 @@ import styles from "../styles/Pricing.module.css";
 
 const Pricing = () => {
   return (
-    <div className="globalGradient">
-      <div>Pricing</div>
-      <div className={styles.catchphrase}></div>
-      <div className={styles.pricesContainer}>
-        <div className={styles.onePriceDiv}></div>
-        <div className={styles.onePriceDiv}></div>
-        <div className={styles.onePriceDiv}></div>
+    <div className="globalGradient pricing">
+      <div className="container">
+        <div className={styles.titleContainer}>
+          <h1>Pricing</h1>
+        </div>
+        <div className={styles.catchphrase}></div>
+        <div className={styles.pricesContainer}>
+          <div className={styles.onePriceDiv}>Accès gratuit</div>
+          <div className={styles.onePriceDiv}>Prix mensuel</div>
+          <div className={styles.onePriceDiv}>Accès entreprise</div>
+        </div>
       </div>
     </div>
   );
