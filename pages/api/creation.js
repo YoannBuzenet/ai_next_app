dd0ee197aa4bd884797f4662a9ca6f50848143a1
Yoann <ybuzenet@gmail.com>
2021-03-27 13:50:20 +0100

    next endpoint to contact API

--- pages/api/creation.js
+++ pages/api/creation.js
@@ -1,11 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
-export default (req, res) => {
-  res
-    .status(200)
-    .json([
-      "John DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn DoeJohn Doe",
-      "test",
-      "test test test test test test test test test test test test test test test test test test test test test test test test ",
-    ]);
+export default async (req, res) => {
+  const objectToSend = {
+    category: "",
+    lang: "",
+    userInput: "",
+    passphrase: process.env.FRONT_APP_PASSPHRASE,
+  };
+
+  const APIresp = await axios.post(
+    `${process.env.CENTRAL_API_URL}/api/service`,
+    objectToSend
+  );
+
+  res.status(200).json(APIresp.data);
 };
