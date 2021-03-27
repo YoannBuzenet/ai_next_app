271ed342894e0129c3c4cbbb30fafc327d600139
Yoann <ybuzenet@gmail.com>
2021-03-27 23:41:53 +0100

    beginning pricing page

---
+++ pages/pricing.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Pricing.module.css";
+
+const Pricing = () => {
+  return <p>Pricing</p>;
+};
+
+export default Pricing;
