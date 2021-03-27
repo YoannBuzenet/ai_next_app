5b4b0ccc4c7201a5840e6a3179a89fe21c7f6e80
Yoann <ybuzenet@gmail.com>
2021-03-27 21:14:57 +0100

    building intermadiary API endpoint on Nextjs to contact real API

--- pages/api/creation.js
+++ pages/api/creation.js
@@ -1,17 +1,28 @@
 import axios from "axios";
 
 export default async (req, res) => {
+  let idUser;
+  if (req.body.user.provider === "google") {
+    idUser = req.body.user.googleId;
+  }
+
   const objectToSend = {
     category: req.body.categoryID,
-    lang: "",
+    lang: req.body.lang,
     userInput: req.body.userInputs,
     passphrase: process.env.FRONT_APP_PASSPHRASE,
+    idUser: idUser,
+    provider: req.body.user.provider,
   };
 
-  const APIresp = await axios.post(
-    `${process.env.CENTRAL_API_URL}/api/service`,
-    objectToSend
-  );
+  try {
+    const APIresp = await axios.post(
+      `${process.env.CENTRAL_API_URL}/api/service`,
+      objectToSend
+    );
 
-  res.status(200).json(APIresp.data);
+    res.status(200).json(APIresp.data);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 };
