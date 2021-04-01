2e8e826624dac3b7a3997544560e5f0ac92fb90f
Yoann <ybuzenet@gmail.com>
2021-04-01 08:56:02 +0200

    base of text generation page

--- pages/templates/text.js
+++ pages/templates/text.js
@@ -1,11 +1,22 @@
 import { useContext } from "react";
 import SelectedCategoryId from "../../contexts/selectedCategoryContext";
+import { categoriesDefinition } from "../../definitions/categories";
+import styles from "../../styles/Text.module.css";
 
-export default function text() {
+export default function Text() {
   const { selectedCategoryID, setSelectedCategoryID } = useContext(
     SelectedCategoryId
   );
 
   console.log("category selected", selectedCategoryID);
-  return <>text</>;
+  return (
+    <div className="genericBackground">
+      <div className="container80">
+        <div className={styles.textGeneratorContainer}>
+          <div className={styles.leftDiv}>g</div>
+          <div className={styles.rightDiv}>d</div>
+        </div>
+      </div>
+    </div>
+  );
 }
