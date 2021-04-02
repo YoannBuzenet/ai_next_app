9567a342e51273201c0d232287583a169de23125
Yoann <ybuzenet@gmail.com>
2021-04-02 18:17:36 +0200

    improving my account page

--- pages/myAccount.js
+++ pages/myAccount.js
@@ -6,26 +6,34 @@ import styles from "../styles/MyAccount.module.css";
 import UserCheck from "../services/userCheck";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  const isLoggedUser = UserCheck.isUserLogged(session?.user?.isLoggedUntil);
-  if (!isLoggedUser) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-      props: {},
-    };
-  } else {
-    return { props: {} };
-  }
-}
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+//   const isLoggedUser = UserCheck.isUserLogged(session?.user?.isLoggedUntil);
+//   if (!isLoggedUser) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//       props: {},
+//     };
+//   } else {
+//     return { props: {} };
+//   }
+// }
 
 export default function MyAccount() {
   const [session, loading] = useSession();
+  const intl = useIntl();
 
   const handleSignOut = () => {
     signOut({ callbackUrl: process.env.NEXT_PUBLIC_URL_ABSOLUTE_THIS_WEBSITE });
@@ -43,16 +51,75 @@ export default function MyAccount() {
         boxShadow: "none",
       },
     },
+    root: {
+      backgroundColor: "white",
+      flexGrow: 1,
+      boxShadow: 0,
+    },
+    tabs: {
+      backgroundColor: "white",
+      color: "black",
+    },
+    tab: {
+      textTransform: "none",
+      fontSize: "1.6rem",
+    },
+    box: {},
+    typo: {
+      fontSize: "1.4rem",
+    },
   }));
 
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3} className={classes.box}>
+            <Typography className={classes.typo}>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  // Translations
+  const translatedSession = intl.formatMessage({
+    id: "page.myAccount.session",
+    defaultMessage: "Session",
+  });
+  const translatedUsageAndBilling = intl.formatMessage({
+    id: "page.myAccount.useageAndBilling",
+    defaultMessage: "Usage and Billing",
+  });
 
   return (
     <div className={styles.myAccountContainer}>
-      <div className="globalGradient">
+      <div className="">
         <div className="container">
           <main className={styles.main}>
-            <div className="global-white-background">
+            <div className={styles.title}>
               <h1>
                 <FormattedMessage
                   id="page.myAccount.title"
@@ -60,27 +127,63 @@ export default function MyAccount() {
                 />
               </h1>
             </div>
-            <div className="global-white-background marginTop20">
-              <div className={styles.signOutContainer}>
-                <p>
-                  <FormattedMessage
-                    id="page.myAccount.logOutLabel"
-                    defaultMessage="Log out Account"
-                  />
-                </p>
-                <Button
-                  onClick={handleSignOut}
-                  className={classes.button}
-                  variant="contained"
-                  size="large"
-                >
-                  <FormattedMessage
-                    id="page.myAccount.logOugButton"
-                    defaultMessage="Sign Out"
-                  />
-                </Button>
+            <div className={styles.tabContainer}>
+              <div className={classes.root}>
+                <AppBar position="static" elevation={0}>
+                  <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="simple tabs example"
+                    className={classes.tabs}
+                    indicatorColor="primary"
+                  >
+                    <Tab
+                      label={translatedSession}
+                      {...a11yProps(0)}
+                      className={classes.tab}
+                    />
+                    <Tab
+                      label={translatedUsageAndBilling}
+                      {...a11yProps(1)}
+                      className={classes.tab}
+                    />
+                    {/* <Tab
+                      label="Item Three"
+                      {...a11yProps(2)}
+                      className={classes.tab}
+                    /> */}
+                  </Tabs>
+                </AppBar>
+                <TabPanel value={value} index={0}>
+                  <div className={styles.signOutContainer}>
+                    <p>
+                      <FormattedMessage
+                        id="page.myAccount.logOutLabel"
+                        defaultMessage="Log out Account"
+                      />
+                    </p>
+                    <Button
+                      onClick={handleSignOut}
+                      className={classes.button}
+                      variant="contained"
+                      size="large"
+                    >
+                      <FormattedMessage
+                        id="page.myAccount.logOugButton"
+                        defaultMessage="Sign Out"
+                      />
+                    </Button>
+                  </div>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                  Le petit graph !
+                </TabPanel>
+                {/* <TabPanel value={value} index={2}>
+                  
+                </TabPanel> */}
               </div>
             </div>
+            <div className=""></div>
           </main>
         </div>
       </div>
