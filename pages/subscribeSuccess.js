853a23c585f07d5ff4549e1b854615917a8d23a9
Yoann <ybuzenet@gmail.com>
2021-04-08 21:03:56 +0200

    filter on free access and sub access is working + implemented on subscriptionSuccess page

--- pages/subscribeSuccess.js
+++ pages/subscribeSuccess.js
@@ -7,47 +7,43 @@ import styles from "../styles/subscribeSuccess.module.css";
 import { useIntl, FormattedMessage } from "react-intl";
 import UserCheck from "../services/userCheck";
 import BlueCTA from "../components/Base/BlueCTA";
+import { isUserSubscribed } from "../services/userCheck";
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
   const isLoggedUser = UserCheck.isUserLogged(session?.user?.isLoggedUntil);
 
-  console.log("session on server", session);
+  const isUserOnFreeAccess = session?.user?.isOnFreeAccess === 1;
+  const isUserSubd = isUserSubscribed(session?.user?.isSubscribedUntil);
 
-  const isUserOnFreeAccess = session?.user?.isOnFreeAccess;
-  const isSubscribedUntil =
-    session?.user?.isSubscribedUntil !== "" &&
-    "DATE EN UTC SUPERIEURE A AUJOURDHUI";
-
-  // TO DO : filtrer si pas abonné ou si pas accès gratuit
-
-  //   if (!isLoggedUser) {
-  //     return {
-  //       redirect: {
-  //         destination: "/",
-  //         permanent: false,
-  //       },
-  //       props: { session },
-  //     };
-  //   } else {
-  //     return { props: { session } };
-  //   }
-  return { props: { session } };
+  if (isUserOnFreeAccess || isUserSubd) {
+    return { props: { session } };
+  } else {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+      props: { session },
+    };
+  }
 }
 
-export default function subscribeSuccess({ session }) {
-  console.log("sessoin :", session);
+export default function subscribeSuccess() {
+  const [session, loading] = useSession();
+
+  const isUserOnFreeAccess = session?.user?.isOnFreeAccess === 1;
+  const isUserSubd = isUserSubscribed(session?.user?.isSubscribedUntil);
 
   const Intl = useIntl();
+
+  console.log("bro", session);
 
   // TRANSLATIONS
   const translatedHead = Intl.formatMessage({
     id: "page.subscribeSuccess.head",
     defaultMessage: "Success !",
   });
-
-  // si free access
-  // si isSubscribedUntil
 
   return (
     <>
@@ -63,74 +59,80 @@ export default function subscribeSuccess({ session }) {
               defaultMessage="Success !"
             />
           </h1>
-          <div className={styles.textContainer}>
-            <div>
-              <p>
-                <FormattedMessage
-                  id="page.subscribeSuccess.subscribed.youArenowSubscribed"
-                  defaultMessage="You are now subscribed to our services."
+          {/* Sub part */}
+          {isUserSubd && (
+            <div className={styles.textContainer}>
+              <div>
+                <p>
+                  <FormattedMessage
+                    id="page.subscribeSuccess.subscribed.youArenowSubscribed"
+                    defaultMessage="You are now subscribed to our services."
+                  />
+                </p>
+              </div>
+              <div>
+                <p>
+                  <FormattedMessage
+                    id="page.subscribeSuccess.subscribed.youCanUseTheApp"
+                    defaultMessage="You can use the application. If needed, feel free to contact us."
+                  />
+                </p>
+              </div>
+              <div>
+                <p>
+                  <FormattedMessage
+                    id="page.subscribeSuccess.subscribed.followConsumption"
+                    defaultMessage="To follow your word use, please check the 'My Account' tab."
+                  />
+                </p>
+              </div>
+              <div>
+                <BlueCTA
+                  to="/templates"
+                  handleClick={() => {}}
+                  idLabel="page.subscribeSuccess.button.GoToApp"
+                  defaultLabel="Go To App"
                 />
-              </p>
+              </div>
             </div>
-            <div>
-              <p>
-                <FormattedMessage
-                  id="page.subscribeSuccess.subscribed.youCanUseTheApp"
-                  defaultMessage="You can use the application. If needed, feel free to contact us."
+          )}
+          {/* Free Access Part */}
+          {isUserOnFreeAccess && (
+            <div className={styles.textContainer}>
+              <div>
+                <p>
+                  <FormattedMessage
+                    id="page.subscribeSuccess.freeAccess.freeAccess"
+                    defaultMessage="You now have a free access to EasyFlow !"
+                  />
+                </p>
+              </div>
+              <div>
+                <p>
+                  <FormattedMessage
+                    id="page.subscribeSuccess.freeAccess.youCanNowUseTheApp"
+                    defaultMessage="You can use the application, with a limit of 5,000 words. If you have any question, please contact us."
+                  />
+                </p>
+              </div>
+              <div>
+                <p>
+                  <FormattedMessage
+                    id="page.subscribeSuccess.freeAccess.followConsumption"
+                    defaultMessage="To follow your word use, please check the 'My Account' tab."
+                  />
+                </p>
+              </div>
+              <div>
+                <BlueCTA
+                  to="/templates"
+                  handleClick={() => {}}
+                  idLabel="page.subscribeSuccess.button.GoToApp"
+                  defaultLabel="Go To App"
                 />
-              </p>
+              </div>
             </div>
-            <div>
-              <p>
-                <FormattedMessage
-                  id="page.subscribeSuccess.subscribed.followConsumption"
-                  defaultMessage="To follow your word use, please check the 'My Account' tab."
-                />
-              </p>
-            </div>
-            <div>
-              <BlueCTA
-                to="/templates"
-                handleClick={() => {}}
-                idLabel="page.subscribeSuccess.button.GoToApp"
-                defaultLabel="Go To App"
-              />
-            </div>
-          </div>
-          <div className={styles.textContainer}>
-            <div>
-              <p>
-                <FormattedMessage
-                  id="page.subscribeSuccess.freeAccess.freeAccess"
-                  defaultMessage="You now have a free access to EasyFlow !"
-                />
-              </p>
-            </div>
-            <div>
-              <p>
-                <FormattedMessage
-                  id="page.subscribeSuccess.freeAccess.youCanNowUseTheApp"
-                  defaultMessage="You can use the application, with a limit of 5,000 words. If you have any question, please contact us."
-                />
-              </p>
-            </div>
-            <div>
-              <p>
-                <FormattedMessage
-                  id="page.subscribeSuccess.freeAccess.followConsumption"
-                  defaultMessage="To follow your word use, please check the 'My Account' tab."
-                />
-              </p>
-            </div>
-            <div>
-              <BlueCTA
-                to="/templates"
-                handleClick={() => {}}
-                idLabel="page.subscribeSuccess.button.GoToApp"
-                defaultLabel="Go To App"
-              />
-            </div>
-          </div>
+          )}
         </main>
       </div>
     </>
