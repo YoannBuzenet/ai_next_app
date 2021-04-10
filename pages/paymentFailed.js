08f65823251d6fd81131a73b6b24d6036a2eb6ee
Yoann <ybuzenet@gmail.com>
2021-04-10 12:53:25 +0200

    should be OK now

--- pages/paymentFailed.js
+++ pages/paymentFailed.js
@@ -19,20 +19,19 @@ export default function PaymentFailed(props) {
         <main className={styles.main}>
           <h1>payment failed</h1>
           {/* Sub part */}
-          {isUserSubd && (
-            <div className={styles.textContainer}>
-              <div>
-                <p>payment failed</p>
-              </div>
-              <div>
-                <p>payment failed</p>
-              </div>
-              <div>
-                <p>payment failed</p>
-              </div>
-              <div>payment failed</div>
+
+          <div className={styles.textContainer}>
+            <div>
+              <p>payment failed</p>
             </div>
-          )}
+            <div>
+              <p>payment failed</p>
+            </div>
+            <div>
+              <p>payment failed</p>
+            </div>
+            <div>payment failed</div>
+          </div>
         </main>
       </div>
     </>
