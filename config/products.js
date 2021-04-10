53f46fc2ab805aed0b8f2e230fd715ea473cebc2
Yoann <ybuzenet@gmail.com>
2021-04-10 09:47:05 +0200

    adding prod id to stripe products

--- config/products.js
+++ config/products.js
@@ -1,6 +1,15 @@
-const products = {
-  monthly: { stripeId: "price_1IeTgPJrGDLQuXVOUPbmsZrF" },
-  yearly: { stripeId: "price_1IeTh0JrGDLQuXVOHhPd8Ids" },
-};
+let products;
+
+if (process.env.NODE_ENV !== production) {
+  products = {
+    monthly: { stripeId: "price_1IeTgPJrGDLQuXVOUPbmsZrF" },
+    yearly: { stripeId: "price_1IeTh0JrGDLQuXVOHhPd8Ids" },
+  };
+} else {
+  products = {
+    monthly: { stripeId: "price_1IeTh3JrGDLQuXVOEcDyieFq" },
+    yearly: { stripeId: "price_1IeTh3JrGDLQuXVOEcDyieFq" },
+  };
+}
 
 module.exports = { products };
