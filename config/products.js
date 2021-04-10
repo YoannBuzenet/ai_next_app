d208f636a6bcf90afbe22d8cd452ea1e00d1d9f4
Yoann <ybuzenet@gmail.com>
2021-04-10 13:36:25 +0200

    changing stripe product id to be able to test

--- config/products.js
+++ config/products.js
@@ -1,15 +1,12 @@
-let products;
-
-if (process.env.NODE_ENV !== "production") {
-  products = {
-    monthly: { stripeId: "price_1IeTgPJrGDLQuXVOUPbmsZrF" },
-    yearly: { stripeId: "price_1IeTh0JrGDLQuXVOHhPd8Ids" },
-  };
-} else {
-  products = {
-    monthly: { stripeId: "price_1IeTh3JrGDLQuXVOEcDyieFq" },
-    yearly: { stripeId: "price_1IeTh3JrGDLQuXVOEcDyieFq" },
-  };
-}
+// dev - test
+const products = {
+  monthly: { stripeId: "price_1IeTgPJrGDLQuXVOUPbmsZrF" },
+  yearly: { stripeId: "price_1IeTh0JrGDLQuXVOHhPd8Ids" },
+};
+// prod
+// const products = {
+//     monthly: { stripeId: "price_1IeTh3JrGDLQuXVOEcDyieFq" },
+//     yearly: { stripeId: "price_1IeTh3JrGDLQuXVOEcDyieFq" },
+//   };
 
 module.exports = { products };
