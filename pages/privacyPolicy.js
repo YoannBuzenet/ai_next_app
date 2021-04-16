ce5a77c84998f46023ceb0ef33da1257b6fe56d5
Yoann <ybuzenet@gmail.com>
2021-04-16 18:11:11 +0200

    privacy policy fully translated

--- pages/privacyPolicy.js
+++ pages/privacyPolicy.js
@@ -172,32 +172,40 @@ export default function TermsAndConditions() {
           />
         </p>
         <p>
-          The right to rectification – You have the right to request that we
-          correct any information you believe is inaccurate. You also have the
-          right to request that we complete the information you believe is
-          incomplete.
+          <FormattedMessage
+            id="page.privacy.GDPR.par3"
+            defaultMessage="The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete."
+          />
         </p>
         <p>
-          The right to erasure – You have the right to request that we erase
-          your personal data, under certain conditions.
+          <FormattedMessage
+            id="page.privacy.GDPR.par4"
+            defaultMessage="The right to erasure – You have the right to request that we erase your personal data, under certain conditions."
+          />
         </p>
         <p>
-          The right to restrict processing – You have the right to request that
-          we restrict the processing of your personal data, under certain
-          conditions.
+          <FormattedMessage
+            id="page.privacy.GDPR.par5"
+            defaultMessage="The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions."
+          />
         </p>
         <p>
-          The right to object to processing – You have the right to object to
-          our processing of your personal data, under certain conditions.
+          <FormattedMessage
+            id="page.privacy.GDPR.par6"
+            defaultMessage="The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions."
+          />
         </p>
         <p>
-          The right to data portability – You have the right to request that we
-          transfer the data that we have collected to another organization, or
-          directly to you, under certain conditions.
+          <FormattedMessage
+            id="page.privacy.GDPR.par7"
+            defaultMessage="The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions."
+          />
         </p>
         <p>
-          If you make a request, we have one month to respond to you. If you
-          would like to exercise any of these rights, please contact us.
+          <FormattedMessage
+            id="page.privacy.GDPR.par8"
+            defaultMessage="If you make a request, we have one month to respond to you. If you  would like to exercise any of these rights, please contact us."
+          />
         </p>
       </main>
     </>
