6dfe601c161866e7d02fb59ff133b64bb8464f9e
Yoann <ybuzenet@gmail.com>
2021-04-20 08:35:57 +0200

    first version of error handling, still to be plugged in notification system

---
+++ services/errorHandling.js
@@ -0,0 +1,35 @@
+import { FormattedMessage } from "react-intl";
+import React from "react";
+
+function checkErrorStatus(error) {
+  console.log("error from error handling :", error);
+  if (error.response) {
+    console.log("error.response", error.response);
+    if (error.response.status === 401) {
+      return (
+        <FormattedMessage
+          id="general.errorHandling.error401"
+          defaultMessage={`You are not logged anymore. Please log again.`}
+        />
+      );
+    } else if (error.response.status === 403) {
+      return (
+        <FormattedMessage
+          id="general.errorHandling.error403"
+          defaultMessage={`You don't have access to this ressource.`}
+        />
+      );
+    }
+  } else if (error.response.status === 500) {
+    return (
+      <FormattedMessage
+        id="general.errorHandling.error500"
+        defaultMessage={`There has been an error. Please try again later.`}
+      />
+    );
+  }
+
+  return false;
+}
+
+export default { checkErrorStatus };
