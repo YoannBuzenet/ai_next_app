654aa46c9bc4fcd23faf5ca057dcd779432434b5
Yoann <ybuzenet@gmail.com>
2021-04-23 23:15:06 +0200

    login page, cutting page in half for future css

--- pages/login.js
+++ pages/login.js
@@ -38,55 +38,60 @@ export default function Login() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="globalGradient">
-        <div className="container loginPage">
+        <div className="loginPage">
           <main className={styles.main}>
             <div>
-              <h1 className="h1Common">
-                <FormattedMessage
-                  id="page.login.title"
-                  defaultMessage="Sign in or Create your Account"
+              <div className="container">
+                <h1 className="h1Common">
+                  <FormattedMessage
+                    id="page.login.title"
+                    defaultMessage="Sign in or Create your Account"
+                  />
+                </h1>
+                <Image
+                  src="/pictures/googleButtons/size2/btn_google_signin_light_normal_web@2x.png"
+                  alt="Connect With Google"
+                  onClick={handleGoogleClick}
+                  height={92}
+                  width={382}
+                  className={styles.images}
                 />
-              </h1>
-              <Image
-                src="/pictures/googleButtons/size2/btn_google_signin_light_normal_web@2x.png"
-                alt="Connect With Google"
-                onClick={handleGoogleClick}
-                height={92}
-                width={382}
-                className={styles.images}
-              />
-              <div className={styles.agreeingTermsAndServices}>
-                <p>
-                  <FormattedMessage
-                    id="page.login.legalNotice.part1"
-                    defaultMessage="By proceeding, you are agreeing to EasyFlow's "
-                  />
-                  <Link href="/termsAndConditions">
-                    <a target="_blank" className={styles.link}>
-                      <FormattedMessage
-                        id="page.login.legalNotice.part2"
-                        defaultMessage="Terms and Conditions "
-                      />
-                    </a>
-                  </Link>
-                  <FormattedMessage
-                    id="page.login.legalNotice.part3"
-                    defaultMessage="and "
-                  />
-                  <Link href="/privacyPolicy">
-                    <a target="_blank" className={styles.link}>
-                      <FormattedMessage
-                        id="page.login.legalNotice.part4"
-                        defaultMessage="Privacy Policy "
-                      />
-                    </a>
-                  </Link>
-                  <FormattedMessage
-                    id="page.login.legalNotice.part5"
-                    defaultMessage="."
-                  />
-                </p>
+                <div className={styles.agreeingTermsAndServices}>
+                  <p>
+                    <FormattedMessage
+                      id="page.login.legalNotice.part1"
+                      defaultMessage="By proceeding, you are agreeing to EasyFlow's "
+                    />
+                    <Link href="/termsAndConditions">
+                      <a target="_blank" className={styles.link}>
+                        <FormattedMessage
+                          id="page.login.legalNotice.part2"
+                          defaultMessage="Terms and Conditions "
+                        />
+                      </a>
+                    </Link>
+                    <FormattedMessage
+                      id="page.login.legalNotice.part3"
+                      defaultMessage="and "
+                    />
+                    <Link href="/privacyPolicy">
+                      <a target="_blank" className={styles.link}>
+                        <FormattedMessage
+                          id="page.login.legalNotice.part4"
+                          defaultMessage="Privacy Policy "
+                        />
+                      </a>
+                    </Link>
+                    <FormattedMessage
+                      id="page.login.legalNotice.part5"
+                      defaultMessage="."
+                    />
+                  </p>
+                </div>
               </div>
+            </div>
+            <div>
+              <div className="container">right part</div>
             </div>
           </main>
         </div>
