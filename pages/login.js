6434f5691c6b08269fc9a266aed1fdf68bdae273
Yoann <ybuzenet@gmail.com>
2021-04-23 23:51:41 +0200

    signin page CSS renewed

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useSession, getSession, signIn } from "next-auth/client";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Link from "next/link";
 import styles from "../styles/Login.module.css";
 import { useIntl, FormattedMessage } from "react-intl";
@@ -31,67 +31,154 @@ export default function Login() {
     defaultMessage: "Sign In",
   });
 
+  useEffect(() => {
+    var elementToTrigger = class ElementToTrigger {
+      constructor(observer, id) {
+        this.observer = observer;
+        this.id = id;
+        this.init();
+      }
+
+      init() {
+        const targets = document.querySelectorAll(this.id);
+
+        targets.forEach((li) => {
+          this.observer.observe(li);
+        });
+      }
+    };
+
+    var options = {
+      rootMargin: "0px",
+      threshold: 0.2,
+    };
+
+    function callback(entries, classToAdd) {
+      entries.filter((el) => {
+        if (el.isIntersecting) {
+          el.target.classList.add(classToAdd);
+        }
+      });
+    }
+
+    // Classic transition
+
+    let observer = new IntersectionObserver(
+      (entries) => callback(entries, "visible"),
+      options
+    );
+
+    const keypoints = new elementToTrigger(observer, "#animated");
+
+    // Right transition
+
+    let observerTransitionRight = new IntersectionObserver(
+      (entries) => callback(entries, "transitionRight"),
+      options
+    );
+
+    const keypointsTransitionRight = new elementToTrigger(
+      observerTransitionRight,
+      "#animated-right"
+    );
+
+    // Left transition
+
+    let observerTransitionLeft = new IntersectionObserver(
+      (entries) => callback(entries, "transitionLeft"),
+      options
+    );
+
+    const keypointsTransitionLeft = new elementToTrigger(
+      observerTransitionLeft,
+      "#animated-left"
+    );
+
+    return () => {
+      keypoints = null;
+      keypointsTransitionLeft = null;
+      keypointsTransitionRight = null;
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <title>{translatedHead}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="globalGradient">
+      <div className="">
         <div className="loginPage">
           <main className={styles.main}>
-            <div>
-              <div className="container">
-                <h1 className="h1Common">
-                  <FormattedMessage
-                    id="page.login.title"
-                    defaultMessage="Sign in or Create your Account"
-                  />
-                </h1>
-                <Image
-                  src="/pictures/googleButtons/size2/btn_google_signin_light_normal_web@2x.png"
-                  alt="Connect With Google"
-                  onClick={handleGoogleClick}
-                  height={92}
-                  width={382}
-                  className={styles.images}
-                />
-                <div className={styles.agreeingTermsAndServices}>
-                  <p>
+            <div className={styles.leftPartContainer}>
+              <div>
+                <div className="container">
+                  <h1 className="h1Common">
                     <FormattedMessage
-                      id="page.login.legalNotice.part1"
-                      defaultMessage="By proceeding, you are agreeing to EasyFlow's "
+                      id="page.login.title"
+                      defaultMessage="Sign in or Create your Account"
                     />
-                    <Link href="/termsAndConditions">
-                      <a target="_blank" className={styles.link}>
+                  </h1>
+                  <div className={styles.buttonContainer}>
+                    <button
+                      type="button"
+                      className="CTAButton"
+                      onClick={handleGoogleClick}
+                    >
+                      <img
+                        src="/icons/google.svg"
+                        className={styles.googleSvg}
+                      />
+                      <p>
                         <FormattedMessage
-                          id="page.login.legalNotice.part2"
-                          defaultMessage="Terms and Conditions "
+                          id="page.login.CTA.connectWithGoogle"
+                          defaultMessage="Continue with Google"
                         />
-                      </a>
-                    </Link>
-                    <FormattedMessage
-                      id="page.login.legalNotice.part3"
-                      defaultMessage="and "
-                    />
-                    <Link href="/privacyPolicy">
-                      <a target="_blank" className={styles.link}>
-                        <FormattedMessage
-                          id="page.login.legalNotice.part4"
-                          defaultMessage="Privacy Policy "
-                        />
-                      </a>
-                    </Link>
-                    <FormattedMessage
-                      id="page.login.legalNotice.part5"
-                      defaultMessage="."
-                    />
-                  </p>
+                      </p>
+                    </button>
+                  </div>
+                  <div className={styles.agreeingTermsAndServices}>
+                    <p>
+                      <FormattedMessage
+                        id="page.login.legalNotice.part1"
+                        defaultMessage="By proceeding, you are agreeing to EasyFlow's "
+                      />
+                      <Link href="/termsAndConditions">
+                        <a target="_blank" className={styles.link}>
+                          <FormattedMessage
+                            id="page.login.legalNotice.part2"
+                            defaultMessage="Terms and Conditions "
+                          />
+                        </a>
+                      </Link>
+                      <FormattedMessage
+                        id="page.login.legalNotice.part3"
+                        defaultMessage="and "
+                      />
+                      <Link href="/privacyPolicy">
+                        <a target="_blank" className={styles.link}>
+                          <FormattedMessage
+                            id="page.login.legalNotice.part4"
+                            defaultMessage="Privacy Policy "
+                          />
+                        </a>
+                      </Link>
+                      <FormattedMessage
+                        id="page.login.legalNotice.part5"
+                        defaultMessage="."
+                      />
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div>
-              <div className="container">right part</div>
+              <div className="container" id="animated">
+                <img src="illustrations/Digital_marketing_SVG.svg" />
+                <p className={styles.whiteParagraph}>
+                  Easy writing in a few clicks
+                </p>
+              </div>
             </div>
           </main>
         </div>
