f76df355323ef05e558d89cb95c143faa3a26d09
Yoann <ybuzenet@gmail.com>
2021-05-04 13:16:22 +0200

    trying again

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src * 'unsafe-inline'",
+            value:
+              "default-src * 'unsafe-inline'; script-src * 'unsafe-inline'",
           },
         ],
       },
