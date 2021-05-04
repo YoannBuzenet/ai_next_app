c345d30ab6d83d437ac12014e78cf2f0da0e05c2
Yoann <ybuzenet@gmail.com>
2021-05-04 09:31:54 +0200

    trying to add a command on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   distDir: "build",
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            // // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+            key: 'Content-Security-Policy',
+            value:
+              "default-src [your-domains...] 'self' data: 'unsafe-inline'"
+          }
+        ]
+      }
 };
