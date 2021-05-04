6cc1077d433f79f8ce3ee8cd1942f8a230d7f69f
Yoann <ybuzenet@gmail.com>
2021-05-04 12:04:12 +0200

    trying again with next config js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   distDir: "build",
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src * 'self' data: 'unsafe-inline' 'unsafe-eval' *; child-src * 'self' data: 'unsafe-inline' 'unsafe-eval' *; script-src 'unsafe-inline' 'self' https://js.stripe.com/v3",
+          },
+        ],
+      },
+    ];
+  },
 };
