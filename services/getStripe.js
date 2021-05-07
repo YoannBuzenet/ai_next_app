176f355f35c3791d91b312802c2a526dddca29dc
Yoann Buzenet <ybuzenet@gmail.com>
2021-05-07 16:13:41 +0200

    trying to wrap into a try catch to see if it stop crashing on old browsers

--- services/getStripe.js
+++ services/getStripe.js
@@ -6,7 +6,11 @@ import { Stripe, loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+    try {
+      stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+    } catch (e) {
+      console.log("error while initating stripe");
+    }
   }
   return stripePromise;
 };
