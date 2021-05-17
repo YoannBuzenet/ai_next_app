89a2c9ed4b4a1625fbf19c12bf80cab2ab74f397
Yoann <ybuzenet@gmail.com>
2021-05-17 22:31:05 +0200

    adding session check on sub page in admin panel

--- pages/admin/category/index.js
+++ pages/admin/category/index.js
@@ -2,6 +2,27 @@ import React from "react";
 import Breadcrumb from "../../../components/admin/Breadcrumb";
 import styles from "../../../styles/admin/category.module.css";
 import { pathlvl2Category } from "../../../definitions/breadcrumb";
+import { useSession, getSession } from "next-auth/client";
+import UserCheck from "../../../services/userCheck";
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  console.log("session from server", session);
+  const hasAccess = UserCheck.hasAdminAccess(session?.user?.rightsCentralAPI);
+
+  if (!hasAccess) {
+    console.log("user doesnt have access");
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+      props: {},
+    };
+  } else {
+    return { props: { session } };
+  }
+}
 
 const Category = () => {
   return (
